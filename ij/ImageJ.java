00e911ea1ac1aeb6c7f3407cd8b59a38e82ddc8c
Aaron Schain <aaronssjunk@aol.com>
2014-10-08 19:27:45 -0400

    Allow port to be changed after instance made

--- ij/ImageJ.java
+++ ij/ImageJ.java
@@ -719,6 +719,13 @@ public class ImageJ extends Frame implements ActionListener,
 		return port;
 	}
 	
+	public static boolean setPort(int delta) {
+		if (delta>0 && DEFAULT_PORT+delta<65536) {
+			port = DEFAULT_PORT+delta;
+			return !OtherInstance.sendArguments(new String[0]);
+		} else return false;
+	}
+	
 	/** Returns the command line arguments passed to ImageJ. */
 	public static String[] getArgs() {
 		return arguments;
